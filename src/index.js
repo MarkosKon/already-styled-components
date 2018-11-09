d70fc1ecdf4b482ce4e6ae0a9476bed4eefbfdd6
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-11-09 19:45:00 +0200

    Same as before, forgot to add. :D

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import AnchorLink from './components/AnchorLink';
 import Fab from './components/Fab';
 import ProgressBar from './components/ProgressBar';
 import { Container, Row, Column } from './components/Grid';
-import { withScrollTo } from './utils/smoothScrolling';
+import animations from './utils/animations';
+import { withScrollTo, withScrollIntoView } from './utils/smoothScrolling';
 
 export {
-  Button, Navbar, AnchorLink, Fab, ProgressBar, Container, Row, Column, withScrollTo,
+  Button,
+  Navbar,
+  AnchorLink,
+  Fab,
+  ProgressBar,
+  Container,
+  Row,
+  Column,
+  animations,
+  withScrollTo,
+  withScrollIntoView,
 };
