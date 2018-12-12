adbc6f01521ede4664a1353acd7e6b19a6364fcf
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-12-12 13:59:41 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'already-styled-components.js',
+    library: 'already-styled-components',
+    libraryTarget: 'umd',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [require('@babel/plugin-proposal-object-rest-spread')],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new BundleAnalyzerPlugin()],
+  externals: {
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+    },
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
+};
